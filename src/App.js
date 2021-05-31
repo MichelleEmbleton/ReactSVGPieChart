6d85ebfe925cb73221469cf07b899ffc034d7866
MichelleEmbleton <michellelembleton@gmail.com>
2021-05-31 10:55:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SVGChart from './components/SVGChart';
+import './app.css';
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <SVGChart />
+    </div>
+  )
+};
+
+export default App;
